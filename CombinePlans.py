d5f8cdb25d89e547aff6ffb173902118ad2fa703
bayfordbury-observatory <50171257+bayfordbury-observatory@users.noreply.github.com>
2022-01-28 15:35:58 +0000

    Update CombinePlans.py

--- CombinePlans.py
+++ CombinePlans.py
@@ -1,7 +1,7 @@
 #CombinePlans.py - Combine RTML files with different targets into one plan
 #
 # Example usage: 
-# >>python CombinePlans.py StandardStar.rtml Target.rtml
+# >>python CombinePlans.py Output.rtml StandardStar.rtml Target.rtml
 #
 # There is no maximum to the number of files that can be combined.
 # Each file should only contain one plan and one target, otherwise only the first plan and target is used.
